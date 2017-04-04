a4369e79c8a79b3ee4f5b9a8284eb1340325c4d4
aisleypay <paisley.singh@gmail.com>
2017-04-04 16:51:47 -0400

    bonus file names updated

--- bin/run_bonus.rb
+++ bin/run_bonus.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
-require_relative "../lib/api_communicator0.rb"
-require_relative "../lib/command_line_interface0.rb"
+require_relative "../lib/api_communicator_bonus.rb"
+require_relative "../lib/command_line_interface_bonus.rb"
 
 subject = get_subject_from_user
 specific_subject = get_subject_specific_from_user(subject)
